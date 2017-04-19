ec308b413c806609aeda5c427272b32ff41fafa1
Shawn Naquin <shawn.naquin@gmail.com>
2017-04-19 13:05:14 -0400

    - indentation to tabs;

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,19 @@ var gulp = require( 'gulp' ),
 	gutil = require('gulp-util'),
 	ftp = require( 'vinyl-ftp' ),
 	secret = require('./.secret.json'),
-    browserSync = require('browser-sync'),
-    zip = require('gulp-vinyl-zip'),
-    rename = require('gulp-rename'),
-    bump = require('gulp-bump'),
-    fs = require('fs'),
-    package = JSON.parse(fs.readFileSync('./package.json')),
-    projects = JSON.parse(fs.readFileSync('./projects.json')),
-    version = package.version,
+	browserSync = require('browser-sync'),
+	zip = require('gulp-vinyl-zip'),
+	rename = require('gulp-rename'),
+	bump = require('gulp-bump'),
+	fs = require('fs'),
+	package = JSON.parse(fs.readFileSync('./package.json')),
+	projects = JSON.parse(fs.readFileSync('./projects.json')),
+	version = package.version,
 
-    project = projects.kylottospeedometer,
-    projectDir = project.directory,
-    files = project.files,
-    HTMLIndex = project.index;
+	project = projects.kylottospeedometer,
+	projectDir = project.directory,
+	files = project.files,
+	HTMLIndex = project.index;
 
 /* clean
 ----------------------------------------------------------------------------------------------------------------------*/
@@ -26,40 +26,40 @@ gulp.task('clean', function() {
 /* version
 ----------------------------------------------------------------------------------------------------------------------*/
 gulp.task('version', function(){
-  gulp.src('./package.json')
-  .pipe(bump({key: "version"}))
-  .pipe(gulp.dest('./'));
+	gulp.src('./package.json')
+		.pipe(bump({key: "version"}))
+		.pipe(gulp.dest('./'));
 });
 
 /* ftp
 ----------------------------------------------------------------------------------------------------------------------*/
 var connOptions = {
-    host:     secret.host,
-    user:     secret.user,
-    password: secret.pass,
-    log:      gutil.log,
-    debug: 	  true
+	host:     secret.host,
+	user:     secret.user,
+	password: secret.pass,
+	log:      gutil.log,
+	debug: 	  true
 };
 
 gulp.task('cleanremote', function(cb) {
 	var conn = ftp.create(connOptions);
-    return conn.rmdir(secret.remotePath+'zip', function(err){
-        cb();
-    });
+	return conn.rmdir(secret.remotePath+'zip', function(err){
+		cb();
+	});
 });
 
 gulp.task( 'ftp', ['cleanremote'], function () {
 	var conn = ftp.create(connOptions);
 
-    return gulp.src( './build/**/*', { base: './build/', buffer: false } )
-        .pipe( conn.newer( secret.remotePath ) ) // only upload newer files
-        .pipe( conn.dest( secret.remotePath ) );
+	return gulp.src( './build/**/*', { base: './build/', buffer: false } )
+		.pipe( conn.newer( secret.remotePath ) ) // only upload newer files
+		.pipe( conn.dest( secret.remotePath ) );
 });
 
 gulp.task( 'ftpIndex', function () {
 	var conn = ftp.create(connOptions);
-    return gulp.src( './build/index.html', { base: './build/', buffer: false } )
-        .pipe( conn.dest( secret.remotePath ) );
+	return gulp.src( './build/index.html', { base: './build/', buffer: false } )
+		.pipe( conn.dest( secret.remotePath ) );
 });
 
 /* rename
@@ -69,17 +69,17 @@ gulp.task('name', function() {
 		.pipe( rename({
 			suffix: '-'+version,
 			extname: '.zip'
-    	}) )
+		}) )
 		.pipe(gulp.dest('./build/zip-version'));
 });
 
 gulp.task('moveindex', ['clean'], function() {
-  return gulp.src(projectDir+'/'+HTMLIndex)
-    .pipe( rename({
-      basename: 'index',
-      extname: '.html'
-    }) )
-    .pipe(gulp.dest('./build'));
+	return gulp.src(projectDir+'/'+HTMLIndex)
+		.pipe( rename({
+			basename: 'index',
+			extname: '.html'
+		}) )
+		.pipe(gulp.dest('./build'));
 });
 
 /* build
@@ -88,14 +88,14 @@ gulp.task('build', ['clean', 'moveindex'], function() {
 	return files.forEach(function(obj) {
 		gulp.src( obj+'gulpfile.js', { read: true } )
 			.pipe( chug() )
-	        .pipe(gutil.noop());
+			.pipe(gutil.noop());
 	});
 });
 
 /* serve
 ----------------------------------------------------------------------------------------------------------------------*/
 gulp.task('serve', function () {
-  browserSync({ server: {
-      baseDir: './build/'
-    }});
+	browserSync({ server: {
+		baseDir: './build/'
+	}});
 });
