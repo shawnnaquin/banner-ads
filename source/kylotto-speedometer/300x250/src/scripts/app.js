24c2284d3577d7cafd032a7d68cb2044b245cbed
Shawn Naquin <shawn.naquin@gmail.com>
2017-04-20 16:27:46 -0400

    versioned and finished ads

--- source/kylotto-speedometer/300x250/src/scripts/app.js
+++ source/kylotto-speedometer/300x250/src/scripts/app.js
@@ -10,6 +10,7 @@ function initAnimation() {
 
 function reset() {
 	document.querySelector('#banner').classList.add('first-frame');
+	document.querySelector('.js-guage-svg').classList.add('run-guage');
 	document.querySelector('.js-needle').classList.add('run-speed');
 	clearTimeouts();
 	runAd();
@@ -29,22 +30,25 @@ function runAd() {
 		hideShow(1,2);
 	}, startAt+(timeOnFrame*1.16666)) );
 
-	timeouts.push( setTimeout( function() {
-		hideShow(2,0);
-		reset();
-	}, startAt+(timeOnFrame*2.16666)) );
+	// timeouts.push( setTimeout( function() {
+	// 	hideShow(2,0);
+	// 	reset();
+	// }, startAt+(timeOnFrame*2.16666)) );
 }
 
 function hideShow(cur,next) {
+
 	document.querySelector('.js-'+layerArray[cur]).style.opacity = 0;
 	document.querySelector('.js-'+layerArray[next]).style.opacity = 1;
+
 	if ( cur === 0 ) {
 		document.querySelector('#banner').classList.remove('first-frame');
 		setTimeout(function(){
 			document.querySelector('.js-needle').classList.remove('run-speed');
+			document.querySelector('.js-guage-svg').classList.remove('run-guage');
 		}, fadeDuration);
 	}
-	// console.log('hs', cur, next);
+
 }
 
 
