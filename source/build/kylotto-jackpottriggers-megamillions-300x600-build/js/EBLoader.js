387aaca2831d76e0afa01b0e78034b28f43f5338
Shawn Naquin <shawn.naquin@gmail.com>
2017-06-20 15:58:33 -0400

    - delete compiled files for jackpot triggers;

--- source/build/kylotto-jackpottriggers-megamillions-300x600-build/js/EBLoader.js
+++
@@ -1,1 +0,0 @@
-function loadServingSysScript(t){document.write("<script src='"+("https:"===document.location.protocol?"https://secure-":"http://")+"ds.serving-sys.com/BurstingScript/"+t+"'></script>")}loadServingSysScript("EBLoader.js");
