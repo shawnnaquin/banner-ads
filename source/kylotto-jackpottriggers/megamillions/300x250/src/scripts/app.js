e369b221b757042979e6b7fb354cf4d8d8ffcb10
Shawn Naquin <shawn.naquin@gmail.com>
2017-06-22 12:51:11 -0400

    - started building KY Lotto - JackPot Triggers - MegaMillions - 300x250;

--- source/kylotto-jackpottriggers/megamillions/300x250/src/scripts/app.js
+++ source/kylotto-jackpottriggers/megamillions/300x250/src/scripts/app.js
@@ -1,77 +1,38 @@
+var showClass = 'show';
 var layerArray = ['layer-one','layer-two','layer-three'];
 var secondFrameStart = 2300;
 var timeOnFrame = 3000;
-var startDeg = -90;  
-var degRange = 180;
-var guageFrame = 0;
-var tickArray = [];
-var rgbRedValue = 255;
 var timeouts;
 
-// t: current time, b: begInnIng value, c: change In value, d: duration
-function easeInOutCubic(t, b, c, d) {
-	if ((t/=d/2) < 1) return c/2*t*t + b;
-	return -c/2 * ((--t)*(t-2) - 1) + b;
-}
-
 function initAnimation() {
-	reset();
+	// getAjax();
+	// reset();
 }
 
-function spawnColor(iter){
-    this.goFrames = 0;
-    this.color = rgbRedValue;
-    this.iteration = iter;
-    this.fillColor = function(){
-		if ( this.goFrames <= 20 ) {
-			document.querySelector('.js-guage-svg > path:nth-child('+String(this.iteration)+')').style.fill = 'rgb('+rgbRedValue+','+this.color+','+this.color+')';
-			this.color = this.color - (rgbRedValue / 20);
-			this.goFrames = this.goFrames + 1;
-		    this.reqAnim = requestAnimationFrame( this.fillColor.bind(this) );
-		} else {
-			this.goFrames = 0;
-			this.color = rgbRedValue;
-			cancelAnimationFrame(this.reqAnim);
-		}
-	};
-}
-
-function engageGuage() {
-	var totalFrames = 100;
-	var currChild = 0;
-	var reqGuage;
-	if ( guageFrame <= totalFrames ) {
-		var deg = startDeg+easeInOutCubic(guageFrame, 0, degRange, totalFrames );
-		var iteration = Math.ceil( easeInOutCubic(guageFrame, 0, document.querySelectorAll('.js-guage-svg > path').length+1, totalFrames ) );
-		document.querySelector('.js-needle').style.transform = 'rotateZ('+deg+'deg)';
-		if ( currChild != iteration ) {
-			tickArray[iteration] = new spawnColor(iteration);
-			tickArray[iteration].fillColor();
-			currChild = iteration;
-		}
-		guageFrame++;
-		reqGuage = requestAnimationFrame(engageGuage);
-	} else {
-		guageFrame = 0;
-		cancelAnimationFrame(reqGuage);
-	}
-}
+// function getAjax() {
+// 	microAjax({
+// 		url: "https://www.kylottery.com/apps/rss/nextjackpots.rss",
+// 		method: "GET",
+// 		success: function(data) {
+// 			console.log(data);
+// 		},
+// 		warning: function(error) {
+// 			console.log('warnining!');
+// 		},
+// 		error: function(error) {
+// 			console.log('error!');
+// 		}
+// 	});
+// }
 
 function reset() {
-
-	Array.prototype.forEach.call(document.querySelectorAll('.js-guage-svg > path'), function(el, i){
-		el.style.fill = '';
-	});
-	document.querySelector('.js-needle').style.transform = 'rotateZ('+startDeg+'deg)';
-	document.querySelector('.js-banner').classList.add('first-frame');
 	timeouts = [];
-	tickArray = [];
 	runAd();
 }
 
 function runAd() {
 
-	engageGuage();
+	hideShow(2,0);
 
 	timeouts.push( setTimeout( function() {
 		hideShow(0,1);
@@ -81,18 +42,28 @@ function runAd() {
 		hideShow(1,2);
 	}, secondFrameStart+(timeOnFrame*1.16666) ) );
 
-	// timeouts.push( setTimeout( function() {
-	// 	hideShow(2,0);
-	// 	reset();
-	// }, secondFrameStart+(timeOnFrame*2.16666) ) );
-
 }
 
 function hideShow(cur,next) {
-	document.querySelector('.js-'+layerArray[cur]).style.opacity = 0;
-	document.querySelector('.js-'+layerArray[next]).style.opacity = 1;
-	if ( cur === 0 ) {
-		document.querySelector('.js-banner').classList.remove('first-frame');
-	}
-}
 
+	var layerOne = ['.js-geta', '.js-bang', '.js-foryour'];
+	var layerTwo = ['.js-layer2-star','.js-the', '.js-megamillions','.js-jackpotishere'];
+	var layerThree = ['.js-layer3-star', '.js-info'];
+	var layerArrays = [layerOne, layerTwo, layerThree];
+
+	// animate layer
+	document.querySelector('.js-'+layerArray[cur]).classList.remove(showClass);
+	document.querySelector('.js-'+layerArray[next]).classList.add(showClass);
+
+	// animnate layer items
+	layerArrays.forEach(function(el,i){
+		el.forEach(function(il) {
+			if ( i !== next ) {
+				document.querySelector(il).classList.remove(showClass);
+			} else {
+				document.querySelector(il).classList.add(showClass);
+			}
+		});
+	});
+
+}
