1fffa950693ea9715728a1821ee9e5f7b64c2f46
Shawn Naquin <shawn.naquin@gmail.com>
2017-09-18 14:56:58 -0400

    - built all house-fire ads;

---
+++ source/servpro-q4-moments/house-fire/970x90/src/scripts/ad.js
@@ -0,0 +1,34 @@
+var fixed = document.getElementById('fixed');
+var van = document.getElementById('van');
+var final = document.getElementById('final');
+
+function init(){
+    cycle();
+}
+
+function cycle() {
+    console.log('starting cycle');
+
+    setTimeout(function() {
+        van.classList.add('drive1');
+    }, 2000);
+
+    setTimeout(function() {
+        fixed.classList.add('fade-in');
+        van.classList.add('drive2');
+    }, 5000);
+
+    setTimeout(function() {
+        van.classList.remove('drive1');
+        van.classList.remove('drive2');
+        final.classList.add('fade-in');
+    }, 10000);
+
+    setTimeout(function() {
+        fixed.classList.remove('fade-in');
+        final.classList.remove('fade-in');
+        cycle();
+    }, 15000);
+}
+
+window.onload = init;
