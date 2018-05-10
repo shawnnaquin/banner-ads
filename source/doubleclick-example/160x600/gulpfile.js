0dac607fd92f53e5a85d050ce85542170487081c
Shawn Naquin <shawn.naquin@gmail.com>
2018-05-10 11:29:49 -0400

    - finish DCM ad;

--- source/doubleclick-example/160x600/gulpfile.js
+++ source/doubleclick-example/160x600/gulpfile.js
@@ -11,7 +11,7 @@ var gulp = require('gulp'),
 	htmlmin = require('gulp-htmlmin'),
 	jslint = require('gulp-jslint'),
 
-	build = '../../../build/doubleclick-example/160x600-build/',
+	build = '../../../build/doubleclick-example-160x600-build/',
 	src = './src/',
 
 	paths = {
@@ -26,12 +26,12 @@ var gulp = require('gulp'),
 // Sass task: Compile SCSS files to CSS
 gulp.task('sass', function () {
 	return gulp.src(paths.sass + '*.scss')
-		.pipe(maps.init())
+		// .pipe(maps.init())
 		.pipe(sass())
-		.on('error', sass.logError )
+		// .on('error', sass.logError )
 		.pipe(autoprefixer())
-		.pipe(maps.write())
-		// .pipe(cssmin())
+		// .pipe(maps.write())
+		.pipe(cssmin())
 		.pipe(gulp.dest(paths.css))
 		.pipe(browserSync.reload({ stream: true })); // Reload browser
 });
@@ -48,11 +48,11 @@ gulp.task('browser-sync', ['default'], function () {
 // Scripts task: Compile TypeScript files to js
 gulp.task('scripts', function () {
   return gulp.src([ paths.scripts + 'polyfill.js', paths.scripts + 'app.js', paths.scripts + 'script.js' ])
-	.pipe(jslint())
-	.pipe(maps.init())
-	.pipe(maps.write())
+	// .pipe(jslint())
+	// .pipe(maps.init())
+	// .pipe(maps.write())
 	.pipe(concat('scripts.js'))
-	// .pipe(uglify())
+	.pipe(uglify())
 	.pipe(gulp.dest(paths.js))
 	.pipe(browserSync.reload({ stream: true })); // Reload browser
 });
--- source/doubleclick-example/160x600/src/scripts/script.js
+++ source/doubleclick-example/160x600/src/scripts/script.js
@@ -2,37 +2,25 @@
 //HTML5 Ad Template JS from DoubleClick by Google
 
 var buttons = {
-    link: 'http://google.com',
     exitBtn: {
       $: '',
       id: 'HTML5_Background_Clickthrough'
     },
-    otherBtn: {
-      $: '',
-      id: 'HTML5_OtherBtn_Clickthrough'
-    }
 };
 
 dcrmInit = function(){
-    buttons.exitBtn.$ = document.querySelector('.js-banner');
-    buttons.otherBtn.$ = document.querySelector('.js-button');
+    buttons.exitBtn.$ = document.querySelector('.js-button');
     addListeners();
     initAnimation();
 }
 
 addListeners = function (){
     buttons.exitBtn.$.addEventListener('click', onExitHandler, false);
-    buttons.otherBtn.$.addEventListener('click', altExitHandler, false);
 }
 
 onExitHandler = function(e){
-    Enabler.exit( buttons.exitBtn.id, buttons.link );
+    Enabler.exit( buttons.exitBtn.id );
     Enabler.counter( buttons.exitBtn.id );
-}
-
-altExitHandler = function(e) {
-    Enabler.exitOverride( buttons.otherBtn.id, buttons.link );
-    Enabler.counter( buttons.otherBtn.id );
     e.stopPropagation();
 }
 
